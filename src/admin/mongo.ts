c07d96a19d87fa1f39fc8033b76765fef0371db5
Tommy Jiang <tianyangj@gmail.com>
2018-07-26 01:49:44 -0700

    tryGetBlock

--- src/admin/mongo.ts
+++ src/admin/mongo.ts
@@ -1,3 +1,4 @@
+import * as util from 'util';
 import * as Eos from 'eosjs';
 import { MongoClient } from 'mongodb';
 
@@ -6,13 +7,29 @@ const eos = Eos({
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906'
 });
 
+const sleep = util.promisify(setTimeout);
+const RETRY = 100;
+
 // 'mongodb://USER:PASSWORD@eosio-shard-00-00-xfxjs.mongodb.net:27017,eosio-shard-00-01-xfxjs.mongodb.net:27017,eosio-shard-00-02-xfxjs.mongodb.net:27017/eos?ssl=true&replicaSet=eosio-shard-0&authSource=admin&retryWrites=true'
 // 'mongodb://USER:PASSWORD@159.65.110.227:27017/eos?authSource=admin'
 const connectionString = 'mongodb://@159.65.110.227:27017/eos?authSource=admin';
 
+async function tryGetBlock(blockNumber) {
+    for (let i = 0; i <= RETRY; i++) {
+        try {
+            return await eos.getBlock(blockNumber);
+        } catch (err) {
+            if (RETRY === i) {
+                throw err;
+            }
+            await sleep(i * 1000);
+        }
+    }
+}
+
 // create block and transactions
 async function createBlock(blockNumber: number, db) {
-    const { transactions: transactionsRaw, ...blockRaw } = await eos.getBlock(blockNumber);
+    const { transactions: transactionsRaw, ...blockRaw } = await tryGetBlock(blockNumber);
     const blocksColl = db.collection('blocks');
     const transactionsColl = db.collection('transactions');
     const block = {
